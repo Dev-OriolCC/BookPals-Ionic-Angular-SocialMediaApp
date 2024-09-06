6d92f627fb613172a524dde3696e162ba5c20ebb
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2024-09-06 13:37:04 -0500

    Created the Post Cards for Tab2 of App

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  isLoading: boolean = true;
 
-  constructor() {}
+  constructor() {
+    
+    setTimeout(() => { 
+      this.isLoading = false;
+    }, 3000);
+
+  }
+
+  handleRefresh(event: { target: { complete: () => void; }; }): void { 
+
+    setTimeout(() => {
+      event.target.complete();
+    }, 0);
+    console.log("Testing")
+  }
 
 }
